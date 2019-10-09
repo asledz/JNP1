7352b8d08566dae3dc2f9d1149afea3a5025af9c
asledz <anita.gaja.sledz@gmail.com>
2019-10-09 15:56:53 +0200

    Minor fixes, added function to convert string to time.

--- zadanie_pierwsze/kasa.cc
+++ zadanie_pierwsze/kasa.cc
@@ -1,15 +1,33 @@
-#include<iostream>
-#include<vector>
-#include<map>
-#include<set>
-#include<string>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
 
 using namespace std;
 
 /* Funkcje użytkowe */
 
+int char_to_int(char num) {
+	return num - '0';
+}
+
 int time_to_minutes (int hour, int minutes) {
 	return 60 * hour + minutes;
+}
+
+// Funkcja konwertująca string z opisem godziny do minut.
+// W przypadku niepoprawnej godziny, zwraca -1.
+int string_to_time (string time) {
+	if(time.size() < 4 || time.size() > 5) {
+		return -1;
+	}
+	int minutes = char_to_int(time[time.size() - 1]) + 10 * char_to_int(time[time.size() - 2]);
+	int hours = char_to_int(time[0]);
+	if(time[1] != ':') {
+		hours = hours * 10 + char_to_int(time[1]);
+	}
+	return time_to_minutes(hours, minutes);
 }
 
 /* Funkcje obsługi rozkładu jazdy */
@@ -25,13 +43,13 @@ vector<map<string, int>> timetable;
 vector<pair<string, pair<double, int>>> tickets;
 
 // Sprawdza, czy dany numer linii kursu tramwajowego zatrzymuje się o danej godzinie na przystanku.
-bool stop_exists (int number_of_the_line, string name_of_the_stop, int hour, int minutes) {
+bool stop_exists (int number_of_the_line, string name_of_the_stop, int time) {
 	// nie istnieje taka linia tramwajowa
 	if(line_id.find(number_of_the_line) == line_id.end() ) {
 		return false;
 	}
 	int id = line_id[number_of_the_line];
-	if(timetable[id][name_of_the_stop] == time_to_minutes(hour, minutes)) {
+	if(timetable[id][name_of_the_stop] == time) {
 		return true;
 	}
 	else {
